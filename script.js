e27032cbb1f501a293e691f1044d3f6d592ac4c4
timalo <tmalo@student.oulu.fi>
2021-08-31 22:15:03 +0300

    HTML/CSS skeleton ready

---
+++ script.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+function Book(title, author, pages, isRead){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.sayInfo = function() {
+        return(`${this.title} by ${this.author}, ${this.pages} pages. readStatus: ${this.isRead}`);
+    }
+}
+
+function addBookToLibrary() {
+    
+}
+
