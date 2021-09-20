3c127589345e85841ca974264d21d96b682ece97
timalo <tmalo@student.oulu.fi>
2021-09-20 14:35:50 +0300

    Added the modal window which looks fine for now, no functionality yet.

--- script.js
+++ script.js
@@ -37,10 +37,39 @@ function updateBookList() {
     myLibrary.forEach(element => {
         var newBookDiv = document.createElement('div');
         newBookDiv.className = 'book';
-        newBookDiv.textContent = `Title: ${element.title}\nAuthor: ${element.author}\nPages: ${element.pages}`; //add the rest later if this works
+        newBookDiv.textContent = `Title: ${element.title}\nAuthor: ${element.author}\nPages: ${element.pages}\nRead status: ${element.isRead}`; 
+        var deleteBtn = document.createElement('button');
+        deleteBtn.className = 'deleteBtn';
+        deleteBtn.textContent = 'Delete';
+        newBookDiv.appendChild(deleteBtn);
         document.querySelector('#bookArea').appendChild(newBookDiv);
     })
 }
 
 //----------------------
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("newBookBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
