30956243ce1aa3a8179a6d77b1ff53f89f680824
timalo <tmalo@student.oulu.fi>
2021-09-20 13:25:34 +0300

    book cards now appearing on book area properly

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-let myLibrary = [];
+let myLibrary = [{
+    title: "testBook",
+    author: "testAuthor",
+    pages: 666,
+    isRead: true,
+    bookId: 1
+},
+{
+    title: "test2Book",
+    author: "second Author",
+    pages: 50,
+    isRead: false,
+    bookId: 2
+}];
 
-function Book(title, author, pages, isRead){
+function Book(title, author, pages, isRead, bookId){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.bookId = bookId;
     this.sayInfo = function() {
         return(`${this.title} by ${this.author}, ${this.pages} pages. readStatus: ${this.isRead}`);
     }
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book);
+    
 }
 
 function NewBookPrompt() {
 
 }
+
+function updateBookList() {
+    document.getElementById('bookArea').innerHTML ="";
+    myLibrary.forEach(element => {
+        var newBookDiv = document.createElement('div');
+        newBookDiv.className = 'book';
+        newBookDiv.textContent = `Title: ${element.title}\nAuthor: ${element.author}\nPages: ${element.pages}`; //add the rest later if this works
+        document.querySelector('#bookArea').appendChild(newBookDiv);
+    })
+}
+
+//----------------------
 
